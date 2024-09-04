8609a5569c7d93efc983e7c5f091fbdd4f955567
Nouby Mohamed <nouby@google.com>
2024-09-04 22:19:51 +0000

    Added EXTRA_ALLOWED_DEPS_TXT to allow arbitrary allowedlist text files that enforces min_sdk_version for apex dependencies to avoid regression
    Test: Ran APEX soong tests and manual test
    Bug: 333868045
    
    Merged-In: I7b0c7f32687d922dde41150f947c1b993998338f
    Change-Id: I7b0c7f32687d922dde41150f947c1b993998338f

--- android/variable.go
+++ android/variable.go
@@ -523,6 +523,8 @@ type ProductVariables struct {
 
 	PartitionVarsForSoongMigrationOnlyDoNotUse PartitionVariables
 
+	ExtraAllowedDepsTxt *string `json:",omitempty"`
+
 	AdbKeys *string `json:",omitempty"`
 }
 
