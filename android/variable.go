c55e68816a9f62680e9cd620c253ba403d4891e9
Jihoon Kang <jihoonkang@google.com>
2024-10-09 20:44:08 +0000

    Import some product definition variables to Soong
    This change imports PRODUCT_DEVICE and PRODUCT_MODEL to Soong. these
    products will be used to generate the android_device module.
    
    Test: m nothing
    Bug: 372486444
    Change-Id: Ie243d030e8f8515cbcb46cf0d1ad45082c14ceb1

--- android/variable.go
+++ android/variable.go
@@ -475,6 +475,8 @@ type ProductVariables struct {
 
 	ProductManufacturer string `json:",omitempty"`
 	ProductBrand        string `json:",omitempty"`
+	ProductDevice       string `json:",omitempty"`
+	ProductModel        string `json:",omitempty"`
 
 	ReleaseVersion          string   `json:",omitempty"`
 	ReleaseAconfigValueSets []string `json:",omitempty"`
