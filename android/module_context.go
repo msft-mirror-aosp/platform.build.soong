e3845057836c42e197c117815088dfac0fe38d2c
Cole Faust <colefaust@google.com>
2025-02-13 12:45:35 -0800

    Make soong-only builds adb-sync compatible
    Historically, the build has put the staging directories for partitions
    in out/target/product. But in soong-only builds, the main staging
    directory is in the out/soong/.intermediates path for the filesystem
    modules, and then later also replicated in out/target/product for
    backwards compatibility with tools like adb sync.
    
    However, adb sync determines which files to push by if they have
    a different timestamp than on device. Since the device images were
    built using a different staging directory than the one adb sync is
    looking at, the have different timestamps, and adb sync syncs all files
    on the device the first time it's run.
    
    To work around this, ensure the files in both staging directories
    have the same timestamp by preserving the timestamp from their
    source file with the `cp -p` flag.
    
    This cl only does that change for real files right now, not symlinks.
    We could also do it for symlinks in the future, but they're more
    complicated and I didn't want to deal with that right now. This means
    the first adb sync will resync all symlinks on the device, but there
    are relatively few of them and they're small so it's not that big of
    an issue.
    
    Long term, we should rewrite adb sync to not depend on staging
    directories. If it looks at the actual module intermediates files,
    it will see a more consistent timestamp.
    
    Bug: 396466053
    Test: Manually ran m sync workflows. Also testing togging soong-only on and off and rebuilding
    Change-Id: Ia0076a2f8e58404c5c3a0bf0d33b09b6846e95bd

--- android/module_context.go
+++ android/module_context.go
@@ -713,6 +713,17 @@ func (m *moduleContext) installFile(installPath InstallPath, name string, srcPat
 				implicitDeps = append(implicitDeps, extraZip.zip)
 			}
 
+			var cpFlags = "-f"
+
+			// If this is a device file, copy while preserving timestamps. This is to support
+			// adb sync in soong-only builds. Because soong-only builds have 2 different staging
+			// directories, the out/target/product one and the out/soong/.intermediates one,
+			// we need to ensure the files in them have the same timestamps so that adb sync doesn't
+			// update the files on device.
+			if strings.Contains(fullInstallPath.String(), "target/product") {
+				cpFlags += " -p"
+			}
+
 			m.Build(pctx, BuildParams{
 				Rule:        rule,
 				Description: "install " + fullInstallPath.Base(),
@@ -722,7 +733,7 @@ func (m *moduleContext) installFile(installPath InstallPath, name string, srcPat
 				OrderOnly:   orderOnlyDeps,
 				Args: map[string]string{
 					"extraCmds": extraCmds,
-					"cpFlags":   "-f",
+					"cpFlags":   cpFlags,
 				},
 			})
 		}
