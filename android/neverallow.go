a61d453b4e57c082a2af23c2e04a44c2e90a2371
Alyssa Haroldsen <kupiakos@google.com>
2025-03-08 01:08:48 +0000

    Add desktop Trusty rules to the genrule allowlist
    Bug: 398934069
    Test: `m trusty_desktop_vm_x86_64.elf` with topic
    Change-Id: I4456e7b2f98ca1a39517bd6cca3ac29e13eddde4

--- android/neverallow.go
+++ android/neverallow.go
@@ -309,6 +309,10 @@ func createLimitGenruleRules() []Rule {
 		"trusty_tee_package_goog",
 		"trusty_tee_package",
 		// Trusty vm target names
+		"trusty_desktop_vm_arm64.bin",
+		"trusty_desktop_vm_x86_64.elf",
+		"trusty_desktop_test_vm_arm64.bin",
+		"trusty_desktop_test_vm_x86_64.elf",
 		"trusty_test_vm_arm64.bin",
 		"trusty_test_vm_x86_64.elf",
 		"trusty_test_vm_os_arm64.bin",
