544540a3a218924f230413c4b34580a548525e7a
Spandan Das <spandandas@google.com>
2025-03-21 00:04:29 +0000

    Create a validation action to assert `apps` matches prebuilt apex contents
    apkcerts.txt built by Make currently contains an entry app in the tree,
    but is being migrated to be limited to the list of installed apps for
    the device. This unfortunately breaks signing for prebuilt_apex with
    apks, since it silently relied on an entry being created by the source
    `android_app` module.
    
    https://r.android.com/3552560 introduces a new `apps` property that will
    be used to declare this dependency between prebuilt_apex and the cert of
    the android app.
    
    This CL introduces a validation action to ensure that the `apps`
    property is uptodate. It uses apex-ls to get the actual list of
    installed apks, and compares it against the contents of the AppInfo
    providers gathered from `apps` property. To support development in
    minimal branches like art which might not contain the source app, the
    validation action will be triggered when building apkcerts.txt (either
    in Soong or Make), and will not be in the transitive closure of `m
    $my_prebuilt_apex`.
    
    Test: m
    Bug: 399788149
    Change-Id: Idb8ec3ca3bbeee9bd4c09a7d5cd981cc35d765dc

--- filesystem/android_device.go
+++ filesystem/android_device.go
@@ -1107,6 +1107,8 @@ func (a *androidDevice) buildApkCertsInfo(ctx android.ModuleContext, allInstalle
 			}
 		} else if info, ok := android.OtherModuleProvider(ctx, installedModule, java.RuntimeResourceOverlayInfoProvider); ok {
 			apkCerts = append(apkCerts, formatLine(info.Certificate, info.OutputFile.Base(), partition))
+		} else if info, ok := android.OtherModuleProvider(ctx, installedModule, ApkCertsInfoProvider); ok {
+			apkCertsFiles = append(apkCertsFiles, info.ApkCertsFile)
 		}
 	}
 	slices.Sort(apkCerts) // sort by name
@@ -1130,3 +1132,9 @@ func (a *androidDevice) buildApkCertsInfo(ctx android.ModuleContext, allInstalle
 	})
 	return apkCertsInfo
 }
+
+type ApkCertsInfo struct {
+	ApkCertsFile android.Path
+}
+
+var ApkCertsInfoProvider = blueprint.NewProvider[ApkCertsInfo]()
